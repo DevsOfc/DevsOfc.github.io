297a1402411fe427bfac6c3eee9620be05d5fd5f
Valdivino Lopes Rodrigues Junior <valdivino.office@gmail.com>
2021-07-04 00:35:54 -0300

    Conteúdo principal
    Parte institucional do site, ainda em desenvolvimento.

---
+++ pages/calls.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>Developers - Your Dev community</title>
+        <link rel='stylesheet' href='../style/style.css'>
+        <link rel='shortcut icon' href='../media/icons/logosemfundo.png'>
+        <script src='../functions/functions.js'></script>
+    </head>
+    <body>
+        <header>
+            <div id='logo'>
+                <a href='../index.html'>
+                    <img id='icone'src='../media/icons/logosemfundo.png'>
+                    <h1>Developers Community</h1>
+                </a>
+            </div>
+            <div id='menu'>
+                <nav>
+                    <ul>
+                        <a href='eventos.html'><li><h2>Eventos</h2></li></a>
+                        <a href='quem_somos.html'><li><h2>Quem somos</h2></li></a>
+                        <a href='seja_membro.html'><li><h2>Seja membro</h2></li></a>
+                        <a href='login.php'><li><h2>Login</h2></li></a>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <footer>
+            <div id='rodape'>
+                <div id='github'>
+                    <a href='https://github.com/DevsOfc' target='_blank'>   
+                        <img id='git' src='../media/icons/github.png'>
+                        <p>GitHub</p>
+                    </a>
+                </div>
+                <div id='direitos'>
+                    <p>Copyright 2021 Developers Community Official. Todos os direitos reservados.</p>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
